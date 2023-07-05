df3f17d7256c262a33622143b5af2a0ab3d35e02
dong <kdw00y2@gmail.com>
2023-07-05 14:56:05 +0900

    first

---
+++ ch07/s08.java
@@ -0,0 +1,89 @@
+// package ch07;
+
+// // 인터페이스는 '기능'을 의미한다
+// // 여러 클래스에 적용될 수 있다
+// interface 입 {
+//     void cry();
+// }
+
+// interface 눈 {
+//     void see();
+// }
+
+// interface 날개 {
+//     void fly();
+// }
+
+// // 인터페이스들이 모이면 종족의 특성이 될 수 있다
+// abstract class 사람 implements 입, 눈 {
+
+// }
+
+// class 홍길동 extends 사람{
+
+//     @Override
+//     public void cry() {
+        
+//     }
+
+//     @Override
+//     public void see() {
+        
+//     }
+
+
+// }
+
+// class 천사 extends 사람 implements 날개 {
+
+//     @Override
+//     public void cry() {
+        
+//     }
+
+//     @Override
+//     public void see() {
+      
+//     }
+
+//     @Override
+//     public void fly() {
+       
+//     }
+
+// }
+
+// // 인터페이스는 클래스에서 여러개를 구현 할 수 있다
+// class Cat implements 입, 눈 {
+
+//     @Override
+//     public void cry() {
+//         System.out.println("야옹");
+//     }
+
+//     @Override
+//     public void see() {
+//         System.out.println("저 자의 뇌속에 마구니가 가득하구나 내리쳐라");
+//     }
+// }
+
+// class dog implements 입, 눈 {
+
+//     @Override
+//     public void cry() {
+//         System.out.println("멍멍");
+//     }
+
+//     @Override
+//     public void see() {
+//         System.out.println("폐..폐하");
+//     }
+
+// }
+
+// public class s08 {
+//     public static void main(String[] args) {
+
+//     }
+
+// }
---
+++ ch07/s08Quiz.java
@@ -0,0 +1,91 @@
+// package ch07;
+
+// // 인터페이스 / 추상클래스 / 클래스를 이용해서
+// // 스마트폰
+// // 갤럭시 / 아이폰
+// // 갤럭시 - 통화 / 문자 / 빅스비
+// // 아이폰 - 통화 / 문자 / 시리
+// // 갤럭시 - s23
+// // 아이폰 - Iphone14
+
+// interface 통화 {
+//     void call();
+// }
+
+// interface 문자 {
+//     void masage();
+// }
+
+// interface 시리 {
+//     void answer();
+// }
+
+// interface 빅스비 {
+//     void answer();
+// }
+
+// // 인터페이스들이 모이면 종족의 특성이 될 수 있다
+// abstract class 휴대폰 implements 통화, 문자 {
+//      String name;
+// }
+
+
+// class 갤럭시 extends 휴대폰 implements 빅스비 {
+
+//     public 갤럭시(String name){
+//         this.name = name;
+//     }
+
+//     @Override
+//     public void call() {
+//         System.out.println("나는 문어~");
+//     }
+
+//     @Override
+//     public void masage() {
+//         System.out.println("메세지 알람");
+//     }
+
+//     @Override
+//     public void answer() {
+//        System.out.println("부르셨나요");
+//     }
+
+// }
+
+// class 아이폰 extends 휴대폰 implements 시리 {
+
+//     public 아이폰(String name){
+//         this.name = name;
+//     }
+
+//     @Override
+//     public void call() {
+//         System.out.println("나는 문어");
+//     }
+
+//     @Override
+//     public void masage() {
+//         System.out.println("메세지");
+//     }
+
+//     @Override
+//     public void answer() {
+//        System.out.println("시리");
+//     }
+
+   
+
+// }
+
+
+
+
+// public class s08Quiz {
+//     public static void main(String[] args) {
+      
+            
+        
+//     }
+    
+// }
